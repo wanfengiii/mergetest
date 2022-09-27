50407c1b8c422a1ebab03be753dc21bf1f673eaf
wanfeng <wanfengiii@163.com>
2022-09-27 14:38:15 +0800

    init

---
+++ test-part3/src/main/java/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    private static String A = "a";
+    private static String B = "b";
+    private static String D = "d";
+    private static String S = "s";
+    // dddd
+    // test
+    // aaa
+    // bbb
+
+    public static void testA() {
+        System.out.println(A);
+    }
+}
